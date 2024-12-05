20b5cb0386baec9ad336781b2d514bd1512c4f2d
97arelloso <97arelloso>
2024-12-05 12:47:44 +0000

    notebook 1 ejecutado

--- Soluciones/1.- Delta Table (Sol).sql
+++ Soluciones/1.- Delta Table (Sol).sql
@@ -26,6 +26,10 @@ USE SCHEMA schema_alejandro
 
 -- COMMAND ----------
 
+DROP TABLE IF EXISTS schema_alejandro.departamentos_delta
+
+-- COMMAND ----------
+
 CREATE TABLE schema_alejandro.departamentos_delta 
 (ID LONG GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1), NAME STRING, FLOOR INT)
 USING delta
@@ -51,10 +55,6 @@ VALUES
 
 -- COMMAND ----------
 
-DESCRIBE DETAIL schema_alejandro.departamentos_delta
-
--- COMMAND ----------
-
 -- MAGIC %md
 -- MAGIC 4.- Análogo al paso 2, crear la tabla "**departamentos_ext**" con formato parquet, apuntando a "_/mnt/data/departamentos_parquet_" y sobre nuestro esquema con las siguientes columnas:
 -- MAGIC - ID (identificador único, numérico, incremental)
@@ -66,15 +66,15 @@ DESCRIBE DETAIL schema_alejandro.departamentos_delta
 -- COMMAND ----------
 
 CREATE EXTERNAL LOCATION departamentos_location
-URL 's3://databricks-workspace-stack-83c40-bucket/tablas_externas/departamentos_parquet'
-WITH (STORAGE CREDENTIAL ntt_laboratorio_databricks)
+URL 's3://databricks-workspace-stack-be532-bucket/tablas_externas/departamentos_parquet'
+WITH (STORAGE CREDENTIAL nttdata_databricks_lab)
 
 -- COMMAND ----------
 
 CREATE TABLE schema_alejandro.departamentos_external
 (ID LONG , NAME STRING, FLOOR INT)
 USING parquet
-LOCATION "s3://databricks-workspace-stack-83c40-bucket/tablas_externas/departamentos_parquet"
+LOCATION "s3://databricks-workspace-stack-be532-bucket/tablas_externas/departamentos_parquet"
 
 -- COMMAND ----------
 
@@ -98,7 +98,7 @@ VALUES
 -- COMMAND ----------
 
 -- MAGIC %md
--- MAGIC 6.- Actualizar la tabla departamentos_ext para que elimine/actualice la fila de ID 4.
+-- MAGIC 6.- Actualizar la tabla departamentos_external para que elimine/actualice la fila de ID 4.
 
 -- COMMAND ----------
 
@@ -114,36 +114,50 @@ WHERE ID = 4
 -- COMMAND ----------
 
 -- MAGIC %md
--- MAGIC #¿Por qué sucede esto?
+-- MAGIC 7.- Ahora vamos a actualizar la tabla departamentos_delta para que elimine/actualice la fila de ID 4.
+
+-- COMMAND ----------
+
+UPDATE schema_alejandro.departamentos_delta
+SET NAME = 'Cafe'
+WHERE ID = 4
+
+-- COMMAND ----------
+
+DELETE FROM schema_alejandro.departamentos_delta
+WHERE ID = 4
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ## ¿Por qué sucede esto?
 -- MAGIC > La tabla _departamentos_delta_ es **managed** (formato **delta**), mientras que _departamentos_ext_ es **unmanaged** (**externa**). Las tablas externas no aceptan las transacciones ACID, mientras que las managed sí.
 
 -- COMMAND ----------
 
 -- MAGIC %md
--- MAGIC Vamos a ver cómo se comportan los archivos en las tablas externas. Para ello, hay que sacar el detalle de la tabla.
+-- MAGIC 8.- Vamos a ver cómo se comportan los archivos en las tablas externas. Para ello, hay que sacar el detalle de la tabla.
 -- MAGIC
 -- MAGIC [Databricks Describe Detail](https://docs.databricks.com/en/sql/language-manual/sql-ref-syntax-aux-describe-table.html#describe-detail)
 
 -- COMMAND ----------
 
-DESCRIBE DETAIL schema_alejandro.departamentos_ext
+DESCRIBE DETAIL schema_alejandro.departamentos_external
 
 -- COMMAND ----------
 
 -- MAGIC %md
--- MAGIC Ahora, con la ruta sacada en el paso anterior, se va a sacar un listado de los archivos que hay en esa ruta.
+-- MAGIC 9.- Ahora, con la ruta sacada en el paso anterior, se va a sacar un listado de los archivos que hay en esa ruta.
 -- MAGIC
 
 -- COMMAND ----------
 
-LIST 's3://bucket-name/path/to/data'
-%fs ls s3://bucket-name/path/to/data
-dbutils.fs.ls("s3://bucket-name/path/to/data")
+LIST 's3://databricks-workspace-stack-be532-bucket/tablas_externas/departamentos_parquet'
 
 -- COMMAND ----------
 
 -- MAGIC %md
--- MAGIC 6.- Dejando de lado la tabla externa de momento, vamos a ver cómo se comportan los archivos en las tablas delta. Para ello, hay que sacar el detalle de la tabla.
+-- MAGIC 10.- Dejando de lado la tabla externa de momento, vamos a ver cómo se comportan los archivos en las tablas delta. Para ello, hay que sacar el detalle de la tabla.
 -- MAGIC
 -- MAGIC [Databricks Describe Detail](https://docs.databricks.com/en/sql/language-manual/sql-ref-syntax-aux-describe-table.html#describe-detail)
 
@@ -154,20 +168,23 @@ DESCRIBE DETAIL schema_alejandro.departamentos_delta
 -- COMMAND ----------
 
 -- MAGIC %md
--- MAGIC 7.- Ahora, con la ruta sacada en el paso anterior, se va a sacar un listado de los archivos que hay en esa ruta.
+-- MAGIC 11.- Ahora, con la ruta sacada en el paso anterior, se va a sacar un listado de los archivos que hay en esa ruta.
 -- MAGIC
 -- MAGIC [Databricks dbutils.fs](https://learn.microsoft.com/es-es/azure/databricks/dev-tools/databricks-utils#--file-system-utility-dbutilsfs)
 
 -- COMMAND ----------
 
-LIST 's3://bucket-name/path/to/data'
-%fs ls s3://bucket-name/path/to/data
-dbutils.fs.ls("s3://bucket-name/path/to/data")
+LIST 's3://databricks-workspace-stack-be532-bucket/unity-catalog/426896731799702/__unitystorage/catalogs/63de764e-e901-4976-8cc3-740a755771cd/tables/dcb3bc3d-417b-41eb-bfda-69c805a3ffa4'
 
 -- COMMAND ----------
 
 -- MAGIC %md
--- MAGIC 8.- Actualizamos las plantas de todos los departamentos inferiores a la planta 15, aumentando su piso en 1.
+-- MAGIC ## **¿Por** qué pasa esto?
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC 12.- Actualizamos las plantas de todos los departamentos inferiores a la planta 15, aumentando su piso en 1.
 -- MAGIC
 -- MAGIC [Databricks Update Table](https://docs.databricks.com/en/sql/language-manual/delta-update.html)
 
@@ -180,47 +197,35 @@ WHERE floor < 15
 -- COMMAND ----------
 
 -- MAGIC %md
--- MAGIC 9.- Vamos a sacar de nuevo el detalle de la tabla y comparamos el número de archivos.
-
--- COMMAND ----------
-
-DESCRIBE DETAIL schema_alejandro.departamentos_delta
+-- MAGIC 13.- Ya que no podemos sacar un listado de archivos, podemos ver qué ha pasado con los archivos parquet si entramos en la ruta del s3.
 
 -- COMMAND ----------
 
 -- MAGIC %md
--- MAGIC 10.- Tras esta actualización, vamos a volver a sacar un listado de los archivos que hay en la ruta, y comparamos el número de archivos con la ejecución anterior.
-
--- COMMAND ----------
-
--- MAGIC %fs ls ''
-
--- COMMAND ----------
-
--- MAGIC %md
--- MAGIC #¿Por qué sucede esto?
+-- MAGIC ## ¿Por qué sucede esto?
 -- MAGIC
 -- MAGIC > Las tablas Delta, gracias al **Transaction Log** saben qué archivos tienen que leer. Aunque haya más archivos en la ruta, solo se van a leer los correctos. Si se quiere más información sobre el Transaction Log dejo este artículo interesante: https://www.databricks.com/blog/2019/08/21/diving-into-delta-lake-unpacking-the-transaction-log.html
 
 -- COMMAND ----------
 
 -- MAGIC %md
--- MAGIC 11.- Para comprobar esto, vamos a leer el Transaction Log. 
-
--- COMMAND ----------
-
--- MAGIC %fs head '.json'
+-- MAGIC 14.- Para comprobar esto, vamos a leer el Transaction Log. 
 
 -- COMMAND ----------
 
 -- MAGIC %md
--- MAGIC 12.- Ahora vamos a sacar el historial de cambios de la tabla.
+-- MAGIC 15.- Ahora vamos a sacar el historial de cambios de la tabla.
 -- MAGIC
 -- MAGIC [Databricks Describe History](https://www.databricks.com/blog/2019/08/21/diving-into-delta-lake-unpacking-the-transaction-log.html)
 
 -- COMMAND ----------
 
 DESCRIBE HISTORY schema_alejandro.departamentos_delta
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC # HASTA AQUÍ
 
 -- COMMAND ----------
 
