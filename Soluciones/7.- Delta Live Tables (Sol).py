f502102332ce9755cae49ae44277b3cc2d8d6372
97arelloso <97arelloso>
2024-12-10 11:30:06 +0000

    dlt

--- Soluciones/7.- Delta Live Tables (Sol).py
+++ Soluciones/7.- Delta Live Tables (Sol).py
@@ -23,7 +23,11 @@
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC Ahora vamos a definir la capa "bronze". 
+# MAGIC Ahora vamos a definir la capa "bronze". Van a ser cargas en streaming mediante el Auto Loader de Databricks. Cada tabla va a tener el sufijo "__bronze_" para diferenciar cada capa.
+# MAGIC
+# MAGIC [Databricks Auto Loader](https://docs.databricks.com/en/ingestion/cloud-object-storage/auto-loader/index.html)
+# MAGIC
+# MAGIC [Databricks Auto Loader Options](https://docs.databricks.com/en/ingestion/cloud-object-storage/auto-loader/options.html#common-auto-loader-options)
 
 # COMMAND ----------
 
@@ -56,3 +60,57 @@ list_table = dbutils.fs.ls(volume)
 for table in list_table:
     table_name = table[1].replace("/","")
     bronze_layer(volume, table_name)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+
+# COMMAND ----------
+
+def silver_layer(schema, tableName, pk, sequenceBy, expectations):
+  @dlt.table(
+      name=f"{tableName}_silver",
+      comment=f"Silver layer for {tableName}",
+      table_properties={"layer": "silver", "primary key": pk},
+      expect_all_or_drop=expectations
+  )
+
+  return dlt.apply_changes(
+    target=f"{tableName}_silver",
+    source=f"{tableName}_bronze",
+    keys=pk,
+    sequence_by=col(sequenceBy)
+  )
+
+# COMMAND ----------
+
+tables = {
+    "park-data": {
+        "pk": "park_id",
+        "sequenceBy": "bronze_timestamp"
+        "expectations": {
+            "valid_pk": "park_id is NOT NULL"
+        }
+    },
+    "squirrell-data": {
+        "pk": "squirrell id",
+        "sequenceBy": "bronze_timestamp"
+        "expectations": {
+            "valid_pk": "squirrell id is NOT NULL"
+        }
+    },
+    "stories": {
+        "pk": "park_id",
+        "sequenceBy": "bronze_timestamp",
+        "expectations": {
+            "valid_pk": "park_id is NOT NULL"
+        }
+    }
+}
+
+for table, config in tables.items():
+    primaryKey = config["pk"]
+    sequenceBy = config["sequenceBy"]
+    expectations = config["expectations"]
+    silver_layer(schema, table, primaryKey, sequenceBy, expectations)
