89d73043fc8eac2a90e8789748b26c281fb2fcd9
97arelloso <97arelloso>
2024-12-12 16:32:01 +0000

    commit

---
+++ Soluciones/5.1.- Workflows (Sol).py
@@ -0,0 +1,22 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC 1.- Este notebook se ejecutará desde el Workflow. Desde el workflow pasaremos la variable "tabla", sobre la cual ejecutaremos un conteo.
+# MAGIC
+# MAGIC [Get task parameter](https://docs.databricks.com/en/dev-tools/databricks-utils.html#widgets-utility-dbutilswidgets)
+
+# COMMAND ----------
+
+tabla = dbutils.widgets.get("tabla")
+conteo = spark.sql(f"select count(*) from {tabla}")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 2.- Ahora vamos a guardar ese resultado en una variable para pasársela a otra actividad/task. También vamos a pasar el nombre de la tabla.
+# MAGIC
+# MAGIC [Share information between tasks](https://docs.databricks.com/en/jobs/share-task-context.html)
+
+# COMMAND ----------
+
+dbutils.jobs.taskValues.set(key = "tabla", value = tabla)
+dbutils.jobs.taskValues.set(key = "conteo", value = conteo.first()[0])
